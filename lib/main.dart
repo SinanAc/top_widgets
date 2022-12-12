a61ae5e2fea7ba653e77b8fb56b8228ea2b633cb
Sinan <sinanac124@gmail.com>
2022-12-12 23:44:29 +0530

    first one added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:top_widgets/widgets/first.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,11 +10,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      //home: MyHomePage(),
+    return const MaterialApp(
+      home: FirstWidget(),
     );
   }
 }
